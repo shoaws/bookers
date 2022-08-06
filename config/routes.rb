c15acd956b32188fb1f93d3939bcd9fe77f7df0a
shoaws <108819031+shoaws@users.noreply.github.com>
2022-08-06 09:37:04 +0000

    [Addコントローラー名修正]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'books' => 'lists#create'
-  get 'lists/show'
-  get 'books' => 'lists#index'
-  get 'lists/edit'
   get 'homes/top'
-  root to: 'lists#new'
+  root to: 'books#new'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
